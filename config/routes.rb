35345cf870a57fa817088d70e82717a1a3b08b48
Francisco Gonzalez <negrocu@MacBook-Pro-de-Francisco.local>
2018-06-12 10:55:19 -0400

    Se agrega autenticacion

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v2 do
       resources :nodes, :sensors, :measures
+      post 'authenticate', to: 'authentication#authenticate'
     end
   end
 end
