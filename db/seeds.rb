277cc8325882a2b9148e409040bdf8b71569cbf9
Francisco Gonzalez <negrocu@MacBook-Pro-de-Francisco.local>
2018-07-19 18:53:12 -0400

    se agregan sensores a seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sensor1 = Sensor.create(:name => 'humidity', :description => 'sensor de humedad', :units => '%')
+sensor2 = Sensor.create(:name => 'temperature', :description => 'sensor de temperatura', :units => 'Cº')
